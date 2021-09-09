fdea51b2974e25267ccb6ca636ea8f181ebd2550
Kacper Pijacki <kpxdi@protonmail.com>
2021-09-09 18:42:41 +0200

    poprawka odczytu pamięci RAM optymalizacja generowania tekstów

--- app/preload.js
+++ app/preload.js
@@ -1,13 +1,5 @@
-const formatBytes = (bytes, decimals = 0) => {
-	if (bytes === 0) return "0 Bytes";
-
-	const k = 1024;
-	const dm = decimals < 0 ? 0 : decimals;
-	const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
-
-	const i = Math.floor(Math.log(bytes) / Math.log(k));
-
-	return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
+const formatBytes = (bytes) => {
+    return Number(bytes / (1024 * 1024 * 1024));
 };
 
 const changeIfNewValue = (el, data) => {
@@ -144,41 +136,41 @@ window.addEventListener("DOMContentLoaded", () => {
 		var SerialBarCode = document.querySelector(".barcode");
 
 		const Serial = document.createElement("li");
-		Serial.innerText = "Serial: " + data.system.serial;
+		Serial.innerText = `Serial: ${data.system.serial}`;
 		Serial.onclick = () => viewBarcode(data.system.serial);
 		Serial.className = "click";
 
 		const Model = document.createElement("li");
-		Model.innerText = "Model: " + data.system.model;
+		Model.innerText = `Model: ${data.system.model}`;
 		Model.onclick = () => viewBarcode(data.system.model);
 		Model.className = "click";
 
 		const CPU = document.createElement("li");
-		CPU.innerText = "Procesor: " + data.cpu.brand;
+		CPU.innerText = `Procesor: ${data.cpu.brand}`;
 		CPU.onclick = () => viewBarcode(data.cpu.brand);
 		CPU.className = "click";
 
 		const Disks = document.createElement("li");
-		Disks.innerText = "Dysk" + (data.diskLayout.length === 1 ? ": " : "i: ");
+		Disks.innerText = `Dysk${(data.diskLayout.length === 1 ? ": " : "i: ")}`;
 		data.diskLayout.map((diskData) => {
 			const Disk = document.createElement("ul");
 			const DiskText = document.createElement("li");
-			DiskText.innerText = "PN: " + diskData.serialNum;
+			DiskText.innerText = `PN: ${diskData.serialNum}`;
 			DiskText.onclick = () => viewBarcode(diskData.serialNum);
 			DiskText.className = "click";
 			Disk.appendChild(DiskText);
 
 			const serial = document.createElement("ul");
-			serial.innerText = "Nazwa dysku: " + diskData.name;
+			serial.innerText = `Nazwa dysku: ${diskData.name}`;
 			serial.onclick = () => viewBarcode(diskData.name);
 			serial.className = "click";
 			const sizeType = document.createElement("ul");
-			sizeType.innerText = "Pojemność: " + Math.round(diskData.size / 1000000000) + " " + diskData.type;
-			sizeType.onclick = () => viewBarcode(Math.round(diskData.size / 1000000000) + " " + diskData.type);
+			sizeType.innerText = `Pojemność: ${Math.round(diskData.size / 1000000000)} ${diskData.type}`;
+			sizeType.onclick = () => viewBarcode(`${Math.round(diskData.size / 1000000000)} ${diskData.type}`);
 			sizeType.className = "click";
 
 			const smartStatus = document.createElement("ul");
-			smartStatus.innerText = "SMART status: " + diskData.smartStatus;
+			smartStatus.innerText = `SMART status: ${diskData.smartStatus}`;
 
 			Disk.appendChild(sizeType);
 			Disk.appendChild(smartStatus);
@@ -189,8 +181,8 @@ window.addEventListener("DOMContentLoaded", () => {
 
 		const RAM = document.createElement("li");
 		const RAMText = document.createElement("a");
-		RAMText.innerText = "RAM: " + formatBytes(data.mem.total);
-		RAMText.onclick = () => viewBarcode(formatBytes(data.mem.total));
+		RAMText.id = "RAMText";
+		let RAMTotalSize = 0;
 		RAMText.className = "click";
 		RAM.appendChild(RAMText);
 
@@ -199,13 +191,14 @@ window.addEventListener("DOMContentLoaded", () => {
 			const SerialText = document.createElement("li");
 
 			if (memSlotData.type !== "Empty") {
-				SerialText.innerText = "PN: " + memSlotData.partNum;
+				SerialText.innerText = `PN: ${memSlotData.partNum}`;
 				SerialText.onclick = () => viewBarcode(memSlotData.partNum);
 				SerialText.className = "click";
 				Serial.appendChild(SerialText);
 
 				const SlotSize = document.createElement("ul");
-				SlotSize.innerText = "Rozmiar modułu: " + formatBytes(memSlotData.size);
+				SlotSize.innerText = `Rozmiar modułu: ${formatBytes(memSlotData.size)} GB`;
+				RAMTotalSize += formatBytes(memSlotData.size);
 
 				Serial.appendChild(SlotSize);
 			} else {
@@ -215,8 +208,11 @@ window.addEventListener("DOMContentLoaded", () => {
 			RAM.appendChild(Serial);
 		});
 
+		RAMText.innerText = `RAM: ${RAMTotalSize} GB`;
+		RAMText.onclick = () => viewBarcode(`${RAMTotalSize} GB`);
+
 		const GPUs = document.createElement("li");
-		GPUs.innerText = "Grafik" + (data.graphics.controllers.length === 1 ? "a" : "i") + ":";
+		GPUs.innerText = `Grafik${(data.graphics.controllers.length === 1 ? "a" : "i")}:`;
 		const GPUUl = document.createElement("ul");
 		data.graphics.controllers.map((GPUData) => {
 			const GPU = document.createElement("li");
@@ -228,7 +224,7 @@ window.addEventListener("DOMContentLoaded", () => {
 		GPUs.appendChild(GPUUl);
 
 		const Displays = document.createElement("li");
-		Displays.innerText = "Wyświetlacz" + (data.graphics.displays.length !== 1 ? "e" : "") + ":";
+		Displays.innerText = `Wyświetlacz${(data.graphics.displays.length !== 1 ? "e" : "")}:`;
 		const DisplaysUl = document.createElement("ul");
 		data.graphics.displays.map((DisplayData) => {
 			const Display = document.createElement("li");
@@ -236,7 +232,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
 			const resolution = document.createElement("ul");
 			resolution.innerText = `Rozdzielczość: ${DisplayData.currentResX}x${DisplayData.currentResY}`;
-			resolution.onclick = () => viewBarcode(DisplayData.currentResX + "x" + DisplayData.currentResY);
+			resolution.onclick = () => viewBarcode(`${DisplayData.currentResX}x${DisplayData.currentResY}`);
 			resolution.className = "click";
 
 			Display.appendChild(resolution);
@@ -245,7 +241,7 @@ window.addEventListener("DOMContentLoaded", () => {
 		Displays.appendChild(DisplaysUl);
 
 		const Battery = document.createElement("li");
-		Battery.innerText = "Bateri" + (data.battery.length === 1 ? "a" : "e") + ":";
+		Battery.innerText = `Bateri${(data.battery.length === 1 ? "a" : "e")}:`;
 		const BatteriesUl = document.createElement("ul");
 
 		data.battery.map((BatteryData) => {
